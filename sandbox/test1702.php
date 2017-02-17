764d3cd3cb9baced57179f26f4c136b4f5707962
Zoran Ivanovic <zolevs@gmail.com>
2017-02-17 23:59:00 +0100

    simple comparation

---
+++ sandbox/test1702.php
@@ -0,0 +1,55 @@
+
+<?php
+
+	$a = 15;
+	$b = 36;
+
+	if ($a >= $b){
+		print "$b is less or equal than $a";
+	} else {
+		print "$b is greater than $a ";
+	}
+
+ ?>
+
+
+<?php
+
+	// $a = 25;
+	// $b = 36;
+
+	// if ($a < $b){
+	// 	print "$b is greater than $a";
+	// } else {
+	// 	print "$b is less than $a";
+	// }
+
+ ?>
+
+
+<?php
+
+	// $a = "A";
+	// $b = "a";
+
+	// if ($a == $b){
+	// 	print "$b not equal $a";
+	// } else {
+	// 	print "$b and $a are not equal";
+	// }
+
+ ?>
+
+
+<?php
+
+	// $a = 25;
+	// $b = 36;
+
+	// if ($a !== $b){
+	// 	print "$b not equal $a";
+	// } else {
+	// 	print "$b and $a are not equal";
+	// }
+
+ ?>
