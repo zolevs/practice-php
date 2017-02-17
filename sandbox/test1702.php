13e572cf711802c7a6fd98cb7029565e276bae52
Zoran Ivanovic <zolevs@gmail.com>
2017-02-18 00:55:29 +0100

    simple typing php

--- sandbox/test1702.php
+++ sandbox/test1702.php
@@ -1,14 +1,126 @@
+<?php
+
+// // FOREACH loop for array
+
+// foreach ($variable as $key => $value) {
+// 	# code...
+// }
+
+
+
+// // WHILE loop
+// 	$i = 1;
+
+// 	while ($i <= 10) {
+// 		print "$i times<br/>";
+// 		$i++;
+// 	};
+
+ ?>
+
+
 
 <?php
 
-	$a = 15;
-	$b = 36;
+	// // Arrays
 
-	if ($a >= $b){
-		print "$b is less or equal than $a";
-	} else {
-		print "$b is greater than $a ";
-	}
+	// $class_array[0] = "CS122";
+	// $class_array[1] = "Programming concepts 1";
+	// $class_array[2] = "123";
+	// $class_array[3] = "Mandatory";
+	// $class_array[4] = "12";
+
+	// $class_array2 = array ("CS122", "Programming Concepts 2","123","Mandatory","12");
+
+	// $class_array3["class number"] = "cs123";
+	// $class_array3["clas name"] = "Programming concepts 2";
+	// $class_array3["room"] = "123";
+	// $class_array3["Number of classes"] = "12";
+
+ ?>
+
+
+<?php
+	// // Function
+
+	// function display_names($first_name, $last_name = "none") {
+	// 	print "Your first name is $first_name"."<br/>";
+	// 	if($last_name != "none") {
+	// 		print "Your last name is $last_name";
+	// 	}
+	// }
+
+	// display_names("James");
+	// display_names("Jovanka","Broz");
+
+ ?>
+
+
+<?php
+	// // Switch
+
+	// $a = 36;
+
+	// switch($a) {
+	// 	case 10:
+	// 		print "10";
+	// 		break;
+	// 	case 20:
+	// 		print "20";
+	// 		break;
+	// 	case 30:
+	// 		print "30";
+	// 		break;
+	// 	default:
+	// 		print "Number was not found.";
+	// };
+
+ ?>
+
+
+<?php
+
+	// $a = 36;
+	// $b = 16;
+
+	// print $a == $b ? "They are equal":"They are not equal.";
+
+ ?>
+
+
+<?php
+
+	// logic operator
+
+	// $a=25; $b=25; $c=25; $d=35;
+	// if ($a == $b OR $c==$d){
+	// 	print "Everything is equal";
+	// } else {
+	// 	print "Someone is not equal";
+	// }
+	// // Only one side of operation has to be TRUE for the complete expression to be true.
+
+	// $a=25; $b=25; $c=25; $d=35;
+	// if ($a == $b and $c==$d){
+	// 	print "Everything is equal";
+	// } else {
+	// 	print "Someone is not equal";
+	// }
+
+
+ ?>
+
+
+<?php
+
+	// $a = 15;
+	// $b = 36;
+
+	// if ($a >= $b){
+	// 	print "$b is less or equal than $a";
+	// } else {
+	// 	print "$b is greater than $a ";
+	// }
 
  ?>
 
