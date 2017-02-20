364b0029f2a178cd5f85207f8e77de0c04cf0eb4
Zoran Ivanovic <zolevs@gmail.com>
2017-02-20 22:48:51 +0100

    _GET

---
+++ sandbox/test2002_3.php
@@ -0,0 +1,26 @@
+
+<form action="test2002_3.php" method="GET">
+	Name:<br/><input type="text" name="name"><br/>
+	Age: <br/><input type="text" name="age"><br>
+	<!-- <input type="hidden" name="submitted" value="true"> -->
+	<input type="submit" value="submit">
+
+</form>
+
+
+<?php
+// Test with $_GET
+
+$name = $_GET['name'];
+$age = $_GET['age'];
+//
+
+if (isset($name) && isset($age)){
+	if(!empty($name)&&!empty($age))
+	echo $name."<br/>";
+	echo $age."<br/>";
+} else  {
+	echo "error";
+}
+
+ ?>
