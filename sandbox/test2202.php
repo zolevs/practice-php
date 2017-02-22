0ce5a6b58791d48040410c97c3ad51436398f9af
Zoran Ivanovic <zolevs@gmail.com>
2017-02-22 23:23:25 +0100

    array

--- sandbox/test2202.php
+++ sandbox/test2202.php
@@ -1,19 +1,23 @@
 <?php
 
-	$levels = array(
-	1 => array(
-		'name' => 'Level 1',
-		'desc' => 'This is the first description.',
-		),
-	2 => array(
-		'name' => 'Level 2',
-		'desc' => 'This is second description.',
-		),
-	3 => array(
-		'name' => 'Level 3',
-		'desc' => 'This is third description'
-		)
+$GLOBALS['level'] = array(
+		1 => array(
+			'name' => 'Level 1',
+			'desc' => 'This is the first description.',
+			),
+		2 => array(
+			'name' => 'Level 2',
+			'desc' => 'This is second description.',
+			),
+		3 => array(
+			'name' => 'Level 3',
+			'desc' => 'This is third description',
+			'points' => array(1 => 1000),
+			)
 );
+
+
+
 	// echo $levels;
 	// var_dump($levels);
 	// print_r($levels);
@@ -21,5 +25,20 @@
 
 	// echo ($levels[2]['name']);
 
-	echo '<pre>', print_r($levels, true), '</pre>';
+	// echo '<pre>', print_r($level, true), '</pre>';
+
+	// echo $level[3]['points'][1];
+
+// echo $GLOBALS['level'][1]['desc'];
+
+function level_data($level, $data){
+	if (array_key_exists($level, $GLOBALS['level']) === true){
+		return $GLOBALS['level'][$level]['desc'];
+	} else {
+		return false;
+	}
+}
+
+var_dump (level_data (3, 'desc'));
+
  ?>
