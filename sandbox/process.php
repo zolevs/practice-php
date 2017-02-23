787fce801c7914d07d39335617cb96546563627f
Zoran Ivanovic <zolevs@gmail.com>
2017-02-23 22:13:54 +0100

    checkbox and get

---
+++ sandbox/process.php
@@ -0,0 +1,9 @@
+<?php
+
+	if((isset($_GET["agree"]) && $_GET["agree"] == "on")) {
+		echo "Is seted.";
+	// } else {
+	// 	echo "Is not seted.";
+	}
+
+ ?>
---
+++ sandbox/test2302_2.php
@@ -0,0 +1,14 @@
+<form action="process.php" method="get">
+	<p>
+		<input type="checkbox" name="agree"> I agree with Terms and Condition.
+	</p>
+	<p>
+		<input type="submit" value="Register">
+	</p>
+</form>
+
+<?php
+
+
+
+ ?>
