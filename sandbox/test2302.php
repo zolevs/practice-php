35b67001af320ba2e4f78c0fe2a5f57990903b2b
Zoran Ivanovic <zolevs@gmail.com>
2017-02-23 21:08:03 +0100

    cookie

---
+++ sandbox/test2302.php
@@ -0,0 +1,27 @@
+<?php
+
+echo $_COOKIE['name']." is ".$_COOKIE['age'];
+echo "<br/>";
+print_r($_COOKIE);
+
+// $exp = time()+86400;
+
+// setcookie("age", 42, $exp);
+
+
+// echo $_COOKIE["age"];
+
+/*
+
+
+setcookie("name","Zozon",$exp);
+*/
+
+// echo $_COOKIE["name"]
+
+
+// ***** Comment reminder
+// 1-name of the cookie
+// 2-data needs to be stored
+// 3-time to keep cookie (to expire)
+ ?>
