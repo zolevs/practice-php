6f54dbbc38a581229e83f7106e81fbddd5d745e3
Zoran Ivanovic <zolevs@gmail.com>
2017-02-23 22:33:11 +0100

    radio box

---
+++ sandbox/process_2.php
@@ -0,0 +1,11 @@
+<?php
+
+	if(isset($_GET["language"])){
+		$language = $_GET['language'];
+		echo "You selected ".$language;
+		// echo ("Ok");
+	} else {
+		// echo ("nije ok");
+	}
+
+ ?>
---
+++ sandbox/test2302_3.php
@@ -0,0 +1,17 @@
+<form action="process_2.php" method="get">
+	<!-- Naravno mozemo get zameniti sa post metodom -->
+	<p>
+		<input type="radio" name="language" value="PHP">PHP<br/>
+		<input type="radio" name="language" value="JavaScript">JavaScript<br/>
+		<input type="radio" name="language" value="Python">Python<br/>
+	<!-- name nam sluzi da grupisemo  -->
+	</p>
+	<p><input type="submit" value="Submit"></p>
+</form>
+
+<?php
+ // // Radio Box
+
+
+
+ ?>
