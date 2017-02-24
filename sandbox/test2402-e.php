829db6f1cdebcbaaed6b5e28b28c7e85e647065e
Zoran Ivanovic <zolevs@gmail.com>
2017-02-24 22:08:25 +0100

    Retriving data from an image map

---
+++ sandbox/test2402-e.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Retriving Image map data</title>
+</head>
+<body>
+	<br>
+	You clicked at location (
+		<?php echo $_REQUEST['imap_x']. " , ". $_REQUEST['imap_y']; ?>
+	)
+</body>
+</html>
---
+++ sandbox/test2402.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Image maps</title>
+</head>
+<body>
+	<h1>Using image maps</h1>
+	<form method="post" action="test2402-e.php">
+			Click the image:
+		<br>
+			<input type="image" name="imap" src="image-1.jpg">
+	</form>
+
+</body>
+</html>
