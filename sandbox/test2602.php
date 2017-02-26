8647ac050be6da86913f48d78470de64b992a993
Zoran Ivanovic <zolevs@gmail.com>
2017-02-26 15:45:38 +0100

    strlen and substr

--- sandbox/test2602.php
+++ sandbox/test2602.php
@@ -1,12 +1,48 @@
 
 
+<?php
+
+	// // strlen and substr
+
+	// $body = "This is article for some test purpose. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, impedit.";
+	// $body = (strlen($body) > 20) ? substr($body, 0, 20)."...":$body="erased";
+	// echo $body;
+
+ ?>
+
 
 <?php
-	// String length
 
-	$test = "This is another string";
-	$length = strlen($test);
+	// $name = "0123456This is long sentence.";
+	// echo substr($name, 0, 4);
 
-	echo "This string is $length characters long.";
+ ?>
+
+
+<?php
+	// // String length
+
+	// $input = $_GET['Name'];
+	// $max_len = 10;
+
+	// if(strlen($input) > $max_len){
+	// 	echo "Sorry, too long";
+	// } else {
+	// 	echo "Ok.";
+	// };
+
+ ?>
+
+
+
+
+
+<?php
+	// // String length
+
+	// $test = "This is another string";
+	// $length = strlen($test);
+
+	// echo "This string is $length characters long.";
 
  ?>
