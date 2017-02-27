3937ad03e2a63527f66c83ce971f95843945205e
Zoran Ivanovic <zolevs@gmail.com>
2017-02-27 21:31:15 +0100

    practice-php string function

--- sandbox/test2702.php
+++ sandbox/test2702.php
@@ -14,6 +14,27 @@
 // // substr_replace
 
 
+$search = "My name is Zoran. What is your name?";
+$result = substr_count($search,"Zoran");
+// // substr_count(string, to serach, start, end)
+echo $result;
+
+$result = substr_replace($search, "Zole", 11, 5);
+echo $result;
+
+
+// $string = "SMALL LETTERS to capitals AND reverse";
+
+// echo strtoupper($string)."<br/>";
+
+// $stored = strlen($string);
+
+// echo strtolower($string);
+
+
+// echo strrev("My text.");
+// // text reverse
+
 
 
 
@@ -38,7 +59,6 @@
 // $length = strlen($string);
 
 // echo mb_substr($string, 0, $length-2);
-
 
 
 
