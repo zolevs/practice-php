5ff600d5f745559111b789b21778e72d803973f2
Zoran Ivanovic <zolevs@gmail.com>
2017-02-28 23:55:52 +0100

    some styling and playing around with minor changes

--- simplePage/pages/contactus.inc.php
+++ simplePage/pages/contactus.inc.php
@@ -1,4 +1,6 @@
 <h3>Contact</h3>
 <br>
-<p>Street 21st</p>
-<p>Big Kupci</p>
+<p>Street Tamka 16</p>
+<p>Warsaw, Poland</p>
+<p><strong>Phone: +48 2 28 04 10 to 13</strong></p>
+<p><strong>Fax: +48 22 04 14</strong></p>
