68cff52e25c5bd8d29d99a07cf5966981edd1fe2
Zoran Ivanovic <zolevs@gmail.com>
2017-03-04 00:26:33 +0100

    array is populated with data

--- sandbox/test0403.php
+++ sandbox/test0403.php
@@ -1,12 +1,30 @@
 <?php
 
 	$descriptionMembers = array (
-		array (
+		array(
 			'name' => 'Babaroga',
 			'position' => 'Manager',
-			'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint illum officiis architecto.',
+			'bio' => 'Orem ipsum dolor sit amet, consectetur adipisicing elit. Sint illum officiis architecto.',
 			'img' => 'head_1'
 			),
+		array(
+			'name' => 'Samojed',
+			'position' => 'Executive',
+			'bio' => 'Dolor sit amet, consectetur adipisicing elit. Mollitia impedit aut tempora reiciendis, repellat iste saepe, optio facilis?',
+			'img' => 'head_2'
+			),
+		array(
+			'name' => 'Papadubi',
+			'position' => 'Manager',
+			'bio' => 'Ipsum dolor sit amet, consectetur adipisicing elit. Placeat ipsum suscipit dolorum vitae veniam quam!',
+			'img' => 'head_3'
+			),
+		array(
+			'name' => 'Oparaci',
+			'position' => 'Chef',
+			'bio' => 'Krem ipsum dolor sit amet, consectetur adipisicing elit. Veniam veritatis, nesciunt velit?',
+			'img' => 'head_4'
+			)
 		);
 ?>
 	<?php
