d6a9d1f09c41a1f3d4425f35166893aa93f23f79
Zoran Ivanovic <zolevs@gmail.com>
2017-03-06 22:03:48 +0100

    just for fun ;)

---
+++ sandbox/test0603.php
@@ -0,0 +1,23 @@
+<?php
+
+	$number = 23.24;
+	$number2 = 1.84;
+	$number*= $number2;
+	echo "$".$number."<br/>";
+
+	// // Zaokruzuje na dva decimalna mesta
+
+	echo "$".sprintf("%.2f",$number);
+
+
+ ?>
+
+
+<?php
+
+	// // (int) - integer
+
+	// $myInt = 10;
+	// echo (int)($myInt/3);
+
+ ?>
