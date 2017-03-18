14d3c5e5ef9f0312fb951235410a4a0b97b23478
Zoran Ivanovic <zolevs@gmail.com>
2017-03-18 19:56:03 +0100

    konekcija na bazu

---
+++ login_again/config/konekcija.php
@@ -0,0 +1,27 @@
+<?php
+
+	// dodela parametara
+	$server = "localhost";
+	$username_server = "root";
+	$password_server = "";
+
+	$baza = "baza_podataka";
+
+	$konekcija = mysql_connect("$server","$username_server","$password_server");
+	if ($konekcija){
+		echo "Uspesna konekcija na server."."<br/>";
+		$selekcija = mysql_select_db($baza, $konekcija);
+			if ($selekcija){
+				echo "Selekcija BAZE je uspesna"."<br/>";
+			}else{
+				echo "Selekcija BAZE je ne uspesna"."<br/>";
+			}
+	} else {
+		echo "Neuspesna konekcija na server."."<br/>";
+	}
+
+	// ispitivanje postojanja konekcije
+		// selekcija baze podataka
+
+
+ ?>
