d20547633c513265b097015792411ccd173a9450
Zoran Ivanovic <zolevs@gmail.com>
2017-03-18 21:20:05 +0100

    registration form

--- login_again/registracija/index.php
+++ login_again/registracija/index.php
@@ -1,5 +1,31 @@
-<?php
-
-	echo "Iz registracije.";
-
- ?>
+<form action="index.php?opcija=registracija" method="POST">
+	<table>
+		<tr>
+			<th colspan="2">Registracija</th>
+		</tr>
+		<tr>
+			<td>username</td><td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>password</td><td><input type="text" name="password"></td>
+		</tr>
+		<tr>
+			<td>email</td><td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>ime</td><td><input type="text" name="ime"></td>
+		</tr>
+		<tr>
+			<td>prezime</td><td><input type="text" name="prezime"></td>
+		</tr>
+		<tr>
+			<td>datum rodjenja</td><td><input type="date" name="datum"></td>
+		</tr>
+		<tr>
+			<td>slika</td><td><input type="file" name="profil"></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" name="reg" value="Registruj se"></th>
+		</tr>
+	</table>
+</form>
