c870e57e145b09454cf8c9859f90dd4e181e74bd
Zoran Ivanovic <zolevs@gmail.com>
2017-03-24 22:02:31 +0100

    string delimiters

---
+++ sandbox/delimiters.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		String Delimiters
+	</title>
+</head>
+<body>
+	<h3>String Delimiters</h3>
+	<?php
+
+		$firstName = "Sometext";
+
+		print "<p>\"$firstName\", I sad.\"Don't read this again.\"</p>";
+
+		echo "<br/>";
+
+		print '<p> $firstName </p>';
+
+		echo "<br/>";
+
+		print "<p> $firstName </p>";
+	 ?>
+</body>
+</html>
