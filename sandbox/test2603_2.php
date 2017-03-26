6cd0a346348bcea3f7ffecd3a9f697f85788bdb2
Zoran Ivanovic <zolevs@gmail.com>
2017-03-26 19:47:10 +0200

    comparison

--- sandbox/test2603_2.php
+++ sandbox/test2603_2.php
@@ -17,16 +17,11 @@
 		$yearsSlept = number_format(($hours/24) * $age, 2);
 		print "<p>Your name is $firstname and you are $age years old</p>";
 		print "<p> You slept $yearsSlept till now.</p>";
-		if ($age > 40){
+		if ($age > 40 && $yearsSlept > 10){
 			print "<p> Better start planning for Retirement! </p>";
-			if ($yearsSlept > 10) {
-					print "You slept too much.";
-			} else {
-				print "Yes, I mean it.";
-			}
-		} else
-		{
-			print "<p>Time is on your side. </p>";
+			print "You slept too much.";
+		} else	{
+			print "Time <em>might</em> be on your side, or maybe you <em>sleep</em> too much.";
 		}
 		print "<p> - END - </p>";
 	 ?>
