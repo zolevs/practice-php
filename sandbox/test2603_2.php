09ddb4b72cbfaf6b8c1ce684a0ecbb929c301789
Zoran Ivanovic <zolevs@gmail.com>
2017-03-26 19:02:24 +0200

    nested

--- sandbox/test2603_2.php
+++ sandbox/test2603_2.php
@@ -9,18 +9,22 @@
 
 		$firstname = $_POST['firstname'];
 		$birthyear = $_POST['birthyear'];
+		$hours = $_POST['hours'];
 
 		$currentYear = date('Y');
 
 		$age = $currentYear - $birthyear;
-
+		$yearsSlept = number_format(($hours/24) * $age, 2);
 		print "<p>Your name is $firstname and you are $age years old</p>";
-
-		if ($age > 40)
-		{
+		print "<p> You slept $yearsSlept till now.</p>";
+		if ($age > 40){
 			print "<p> Better start planning for Retirement! </p>";
-		}
-		else
+			if ($yearsSlept > 10) {
+					print "You slept too much.";
+			} else {
+				print "Yes, I mean it.";
+			}
+		} else
 		{
 			print "<p>Time is on your side. </p>";
 		}
