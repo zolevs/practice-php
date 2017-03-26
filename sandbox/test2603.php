d4a4e326201f6b30cbe9dd3600a7c9ea7be81da6
Zoran Ivanovic <zolevs@gmail.com>
2017-03-26 17:49:37 +0200

    simple math in table view

--- sandbox/test2603.php
+++ sandbox/test2603.php
@@ -1,4 +1,10 @@
-<h3>Simple math</h3>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Table</title>
+</head>
+<body>
+	<h3>Simple math</h3>
 
 <?php
 	$apples = $_POST['apples'];
@@ -9,9 +15,28 @@
 	$multipleApples = $apples * 10;
 	$divideApples = $apples / 3;
 	$moduloApples = $apples % 3;
-
-	print "<p> Total numbers is $total </p>";
-	print "<p> Number of divided apples is ".$divideApples;
-	print "<p> Number of divided apples (rounded) ".(number_format($divideApples, 2));
-	print '<p> Modulo '.$moduloApples." apples";
+	$fixed = number_format($divideApples,2);
  ?>
+	<table border='1'>
+		<tr>
+			<th>Description</th>
+			<th>Calculated</th>
+		</tr>
+
+			<?php
+				print "<tr> <td> Total numbers is </td> <td> $total </td></tr> \n";
+				print "<tr> <td> Number of divided apples is </td> <td> $divideApples </td> </tr> \n";
+				print "<tr> <td> Number of divided apples (rounded) </td> <td> $fixed </td> </tr> \n";
+
+				print "<tr> <td> Number of divided apples (rounded) </td> <td>"; print(number_format($divideApples,2)); "  </td> </tr> \n";
+
+				print "<tr> <td> Modulo </td> <td> $moduloApples </td> </tr> \n";
+			?>
+
+
+
+	</table>
+
+
+</body>
+</html>
