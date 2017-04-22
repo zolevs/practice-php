d2ca34c5ed78f10ad60c4281bb0c22d9f3d77bbb
Zoran Ivanovic <zolevs@gmail.com>
2017-04-22 20:56:01 +0200

    find city

---
+++ sandbox/formBasic/homelist.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home list</title>
+	<link rel="stylesheet" type="text/css" href="basic.css">
+</head>
+<body>
+	<div>
+		<a href="page_1.php" border="0">
+			<img src="KingLogo.jpg" alt="King logo">
+		</a>
+	</div>
+
+</body>
+
+</html>
